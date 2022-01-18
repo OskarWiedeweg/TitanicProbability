cd7f2345bfdbe477dda79dfcbf2e2a8b1d46ebfb
OskarWiedeweg <oskarwiedeweg01@gmail.com>
2022-01-18 09:09:26 +0100

    Initial Commit

---
+++ src/main/java/com/example/TitanicProbability/TitanicProbabilityApplication.java
@@ -0,0 +1,13 @@
+package com.example.TitanicProbability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitanicProbabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitanicProbabilityApplication.class, args);
+	}
+
+}
