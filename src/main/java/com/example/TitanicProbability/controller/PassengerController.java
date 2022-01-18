4b249b269cec8ae502fa33053fdbdf01925627b7
OskarWiedeweg <oskarwiedeweg01@gmail.com>
2022-01-18 11:38:07 +0100

    Added pagination

--- src/main/java/com/example/TitanicProbability/controller/PassengerController.java
+++ src/main/java/com/example/TitanicProbability/controller/PassengerController.java
@@ -1,11 +1,11 @@
 package com.example.TitanicProbability.controller;
 
 import com.example.TitanicProbability.dtos.PassengerDTO;
-import com.example.TitanicProbability.models.Passenger;
 import com.example.TitanicProbability.services.PassengerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,8 +18,8 @@ public class PassengerController {
     private PassengerService passengerService;
 
     @GetMapping()
-    public List<PassengerDTO> getAll() {
-        return passengerService.getAllPassengers();
+    public List<PassengerDTO> getAll(@RequestParam(required = false) Integer page, @RequestParam(required = false) Integer limit) {
+        return passengerService.getAllPassengers(page, limit);
     }
 
 }
